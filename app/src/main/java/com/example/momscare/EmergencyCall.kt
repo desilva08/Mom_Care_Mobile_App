0d3c541cc33446267c153f3bfdfea17e2461629c
Deshani-Gamlath <143574089+Deshani-Gamlath@users.noreply.github.com>
2024-06-13 21:50:10 +0530

    Test Commit

--- app/src/main/java/com/example/momscare/EmergencyCall.kt
+++ app/src/main/java/com/example/momscare/EmergencyCall.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class EmergencyCall : AppCompatActivity() {
+    //comment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_emergency_call)
